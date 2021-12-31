20ea31237f530d2c1adecabff33dcecf66d319eb
PhiliPdB <phlpdbrn@gmail.com>
2021-12-31 12:08:06 +0100

    Make display buffer work for tricolors

--- src/epd2in7b/graphics.rs
+++ src/epd2in7b/graphics.rs
@@ -1,29 +1,31 @@
 use crate::epd2in7b::{DEFAULT_BACKGROUND_COLOR, HEIGHT, WIDTH};
-use crate::graphics::{Display, DisplayRotation};
-use embedded_graphics_core::pixelcolor::BinaryColor;
+use crate::graphics::{DisplayRotation, TriDisplay};
+use crate::prelude::TriColor;
 use embedded_graphics_core::prelude::*;
 
 /// Full size buffer for use with the 2in7B EPD
 ///
-/// Can also be manuall constructed:
+/// Can also be manually constructed:
 /// `buffer: [DEFAULT_BACKGROUND_COLOR.get_byte_value(); WIDTH * HEIGHT / 8]`
 pub struct Display2in7b {
-    buffer: [u8; WIDTH as usize * HEIGHT as usize / 8],
+    buffer: [u8; 2 * WIDTH as usize * HEIGHT as usize / 8],
     rotation: DisplayRotation,
 }
 
 impl Default for Display2in7b {
     fn default() -> Self {
         Display2in7b {
-            buffer: [DEFAULT_BACKGROUND_COLOR.get_byte_value();
-                WIDTH as usize * HEIGHT as usize / 8],
+            buffer: [
+                DEFAULT_BACKGROUND_COLOR.get_byte_value();
+                2 * WIDTH as usize * HEIGHT as usize / 8
+            ],
             rotation: DisplayRotation::default(),
         }
     }
 }
 
 impl DrawTarget for Display2in7b {
-    type Color = BinaryColor;
+    type Color = TriColor;
     type Error = core::convert::Infallible;
 
     fn draw_iter<I>(&mut self, pixels: I) -> Result<(), Self::Error>
@@ -31,7 +33,7 @@ impl DrawTarget for Display2in7b {
         I: IntoIterator<Item = Pixel<Self::Color>>,
     {
         for pixel in pixels {
-            self.draw_helper(WIDTH, HEIGHT, pixel)?;
+            self.draw_helper_tri(WIDTH, HEIGHT, pixel)?;
         }
         Ok(())
     }
@@ -43,7 +45,7 @@ impl OriginDimensions for Display2in7b {
     }
 }
 
-impl Display for Display2in7b {
+impl TriDisplay for Display2in7b {
     fn buffer(&self) -> &[u8] {
         &self.buffer
     }
@@ -53,22 +55,32 @@ impl Display for Display2in7b {
     }
 
     fn set_rotation(&mut self, rotation: DisplayRotation) {
-        self.rotation = rotation;
+        self.rotation = rotation
     }
 
     fn rotation(&self) -> DisplayRotation {
         self.rotation
+    }
+
+    fn chromatic_offset(&self) -> usize {
+        WIDTH as usize * HEIGHT as usize / 8
+    }
+
+    fn bw_buffer(&self) -> &[u8] {
+        &self.buffer[..self.chromatic_offset()]
+    }
+
+    fn chromatic_buffer(&self) -> &[u8] {
+        &self.buffer[self.chromatic_offset()..]
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::color::Black;
-    use crate::color::Color;
     use crate::epd2in7b;
     use crate::epd2in7b::{HEIGHT, WIDTH};
-    use crate::graphics::{Display, DisplayRotation};
+    use crate::graphics::DisplayRotation;
     use embedded_graphics::{
         prelude::*,
         primitives::{Line, PrimitiveStyle},
@@ -78,7 +90,7 @@ mod tests {
     #[test]
     fn graphics_size() {
         let display = Display2in7b::default();
-        assert_eq!(display.buffer().len(), 5808);
+        assert_eq!(display.buffer().len(), 11616);
     }
 
     // test default background color on all bytes
@@ -94,12 +106,12 @@ mod tests {
     fn graphics_rotation_0() {
         let mut display = Display2in7b::default();
         let _ = Line::new(Point::new(0, 0), Point::new(7, 0))
-            .into_styled(PrimitiveStyle::with_stroke(Black, 1))
+            .into_styled(PrimitiveStyle::with_stroke(TriColor::Black, 1))
             .draw(&mut display);
 
         let buffer = display.buffer();
 
-        assert_eq!(buffer[0], Color::Black.get_byte_value());
+        assert_eq!(buffer[0], TriColor::Black.get_byte_value());
 
         for &byte in buffer.iter().skip(1) {
             assert_eq!(byte, epd2in7b::DEFAULT_BACKGROUND_COLOR.get_byte_value());
@@ -114,12 +126,12 @@ mod tests {
             Point::new(0, WIDTH as i32 - 8),
             Point::new(0, WIDTH as i32 - 1),
         )
-        .into_styled(PrimitiveStyle::with_stroke(Black, 1))
+        .into_styled(PrimitiveStyle::with_stroke(TriColor::Black, 1))
         .draw(&mut display);
 
         let buffer = display.buffer();
 
-        assert_eq!(buffer[0], Color::Black.get_byte_value());
+        assert_eq!(buffer[0], TriColor::Black.get_byte_value());
 
         for &byte in buffer.iter().skip(1) {
             assert_eq!(byte, epd2in7b::DEFAULT_BACKGROUND_COLOR.get_byte_value());
@@ -135,7 +147,7 @@ mod tests {
             Point::new(WIDTH as i32 - 8, HEIGHT as i32 - 1),
             Point::new(WIDTH as i32 - 1, HEIGHT as i32 - 1),
         )
-        .into_styled(PrimitiveStyle::with_stroke(Black, 1))
+        .into_styled(PrimitiveStyle::with_stroke(TriColor::Black, 1))
         .draw(&mut display);
 
         let buffer = display.buffer();
@@ -143,7 +155,7 @@ mod tests {
         extern crate std;
         std::println!("{:?}", buffer);
 
-        assert_eq!(buffer[0], Color::Black.get_byte_value());
+        assert_eq!(buffer[0], TriColor::Black.get_byte_value());
 
         for &byte in buffer.iter().skip(1) {
             assert_eq!(byte, epd2in7b::DEFAULT_BACKGROUND_COLOR.get_byte_value());
@@ -158,7 +170,7 @@ mod tests {
             Point::new(HEIGHT as i32 - 1, 0),
             Point::new(HEIGHT as i32 - 1, 7),
         )
-        .into_styled(PrimitiveStyle::with_stroke(Black, 1))
+        .into_styled(PrimitiveStyle::with_stroke(TriColor::Black, 1))
         .draw(&mut display);
 
         let buffer = display.buffer();
@@ -166,7 +178,7 @@ mod tests {
         extern crate std;
         std::println!("{:?}", buffer);
 
-        assert_eq!(buffer[0], Color::Black.get_byte_value());
+        assert_eq!(buffer[0], TriColor::Black.get_byte_value());
 
         for &byte in buffer.iter().skip(1) {
             assert_eq!(byte, epd2in7b::DEFAULT_BACKGROUND_COLOR.get_byte_value());
